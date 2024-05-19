869a9ef06557f27145fd2f5c73be83b8af548ce9
Turtel216 <papadimitrios216@gmail.com>
2024-05-19 09:38:14 +0300

    Refactored Entity system

--- Wango-Engine/src/entities/GameObject.java
+++ Wango-Engine/src/entities/GameObject.java
@@ -1,6 +1,6 @@
 package entities;
 
 //Entity super class, containing all attributes shared by all Game Objects
-public abstract class Entity {
+public abstract class GameObject {
     //TODO add file loader, render etc. etc.
 }
