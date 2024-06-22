e6b0d952f831050b4fea265b889f845b59c64224
Turtel216 <papadimitrios216@gmail.com>
2024-06-22 22:38:11 +0300

    Added cmake file

---
+++ Wango-Engine/build/CMakeFiles/wango-game-engine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wango-game-engine.dir/main.cpp.o"
+  "CMakeFiles/wango-game-engine.dir/main.cpp.o.d"
+  "wango-game-engine"
+  "wango-game-engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wango-game-engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
