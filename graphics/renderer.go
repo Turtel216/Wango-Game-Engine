95971245bf8569edf86746e49f0265625a70e3f1
Turtel216 <papadimitrios216@gmail.com>
2024-06-23 00:02:41 +0300

    Added run method to GameRenderer

--- graphics/renderer.go
+++ graphics/renderer.go
@@ -1,10 +1,55 @@
 package graphics
 
+import (
+	"fmt"
+
+	"github.com/Turtel216/Wango-Game-Engine/loader"
+	"github.com/gopxl/pixel/pixelgl"
+	"github.com/gopxl/pixel/v2"
+)
+
 type Render interface {
+	Run()
 }
 
 type GameRenderer struct {
 	sizeX       int
 	sizeY       int
 	window_name string
+}
+
+func NewGameRenderer(sizeX int, sizeY int, window_name string) *GameRenderer {
+	return &GameRenderer{
+		sizeX:       sizeX,
+		sizeY:       sizeY,
+		window_name: window_name,
+	}
+}
+
+func (renderer GameRenderer) Run() {
+	cfg := pixelgl.WindowConfig{
+		Title:  renderer.window_name,
+		Bounds: pixel.R(0, 0, float64(renderer.sizeX), float64(renderer.sizeY)),
+	}
+
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		fmt.Println("Could create window")
+		panic(err)
+	}
+
+	pic, err := loader.LoadImage("img.png")
+	if err != nil {
+		fmt.Println("Could not load image")
+		panic(err)
+	}
+
+	sprite := pixel.NewSprite(pic, pic.Bounds())
+
+	win.Clear()
+	sprite.Draw(win, pixel.IM)
+
+	for !win.Closed() {
+		win.Update()
+	}
 }
--- loader/loader.go
+++ loader/loader.go
@@ -1,4 +1,31 @@
 package loader
 
+import (
+	"image"
+	"os"
+
+	"github.com/gopxl/pixel/v2"
+)
+
+type loader interface {
+	loadImage(string) (pixel.Picture, error)
+}
+
 type GameAssetsLoader struct {
+}
+
+func LoadImage(path string) (pixel.Picture, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return pixel.PictureDataFromImage(img), nil
 }
